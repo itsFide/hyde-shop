a1616c82f1aa7183487d57a48be541359b40c7f4
Sergey <murzak301@gmail.com>
2023-03-20 01:32:20 +0600

    adaptivee main page

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,15 @@
-console.log('fdfdf');
+// menu 
+window.onload = ( ) =>{
+    let burger = document.querySelector('.burger');
+    let menu = document.querySelector('.mobile__menu')
+    // let menuDropdown = menu.querySelectorAll('.has-children')
+    burger.addEventListener('click',()=>{
+        burger.classList.toggle('active')
+        menu.classList.toggle('active')
+        if(menu.classList.contains('active')){
+            menu.style.height = `${menu.scrollHeight}px`
+        } else{
+            menu.style.height = `0px`
+        }
+    })
+}
--- src/js/separateJS/main-page.js
+++ src/js/separateJS/main-page.js
@@ -8,6 +8,23 @@ const catalog = new Swiper('.catalog-slider', {
         nextEl: '.catalog-slider-next',
         prevEl: '.catalog-slider-prev',
     },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        650: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1150: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        }
+    }
 });
 
 const discount = new Swiper('.discount-slider', {
@@ -20,6 +37,31 @@ const discount = new Swiper('.discount-slider', {
         nextEl: '.discount-slider-next',
         prevEl: '.discount-slider-prev',
     },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        700: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        991: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1153: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        }
+    }
 });
 
 const hit = new Swiper('.hit-slider', {
@@ -32,4 +74,29 @@ const hit = new Swiper('.hit-slider', {
         nextEl: '.hit-slider-next',
         prevEl: '.hit-slider-prev',
     },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        700: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        991: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1153: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        }
+    }
 });
