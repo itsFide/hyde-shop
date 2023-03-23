fa1e3494d509bf9ee4700ea851e97d2e4ee03325
Sergey <murzak301@gmail.com>
2023-03-24 02:10:32 +0600

    fixed intro block

--- src/js/separateJS/app.js
+++ src/js/separateJS/app.js
@@ -13,3 +13,24 @@ window.onload = ( ) =>{
         }
     })
 }
+
+let productCounter = document.querySelectorAll('.product-counter')
+if(productCounter){
+    productCounter.forEach(e=>{
+        let plus = e.querySelector('.product-counter__plus')
+        let minus = e.querySelector('.product-counter__minus')
+        let value = e.querySelector('.product-counter__value')
+        let valueNum = parseInt(value.innerText)
+        plus.addEventListener('click',()=>{
+            valueNum++
+            value.innerText = valueNum;
+        })
+        minus.addEventListener('click',()=>{
+            if(valueNum > 1){
+                valueNum--
+                value.innerText = valueNum;
+                console.log(valueNum);
+            }
+        })
+    })
+}
